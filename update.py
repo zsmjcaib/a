6f861699f764d375f30cd5e19fed51773065ca7d
zsmjcaib <1053661724@qq.com>
2022-06-07 16:17:27 +0800

    '1'

---
+++ update.py
@@ -0,0 +1,123 @@
+from typing import Dict
+import efinance as ef
+import pandas as pd
+from multiprocessing import Process
+from datetime import datetime,timedelta
+
+import talib
+import yaml
+import os
+
+from  utils.getMacd import stock_macd
+
+
+
+def update(stock_codes,content,date = 'all'):
+    if date == 'all':
+        now = datetime.now()
+        delta32 = timedelta(days=2000)
+        beg = (now - delta32).date().strftime('%Y%m%d')
+    else:
+        beg = datetime.now().date().strftime('%Y%m%d')
+    for freq in [101]:
+        total = len(stock_codes)
+        each = int(total / 30)
+        each = each if each>0 else total+1
+        start = 0
+        end = each
+        # while end<=total+each:
+        #     df_5: Dict[str, pd.DataFrame] = ef.stock.get_quote_history(stock_codes[start:end], klt=freq, beg=beg)
+        #     for stock_code, df in df_5.items():
+        #         if len(df) == 0:
+        #             continue
+        #         deal(content, df, 'day')
+        df_5: Dict[str, pd.DataFrame] = ef.stock.get_quote_history(stock_codes[start:end], klt=freq, beg=beg)
+        deal(content, df_5, 'day')
+        start += each
+        end += each
+
+def deal(content,df,freq):
+    # normal_path = content['normal_'+str(freq)+'_path']
+    # simple_path = content['simple_'+str(freq)+'_path']
+    # deal_path = content['deal_'+str(freq)+'_path']
+    # line_path = content['line_'+str(freq)+'_path']
+    day_path = content['normal']+str(freq)+'/'
+    code = df.iat[1,1]+'.csv'
+    if not os.path.exists(day_path + code):
+        normal = pd.DataFrame(columns=['date','open','high','low','close','amount','vol'])
+    else:
+        normal = pd.read_csv(day_path + code)
+    df.drop(axis=1, columns=['股票名称', '股票代码','振幅','涨跌幅','涨跌额','换手率'],inplace=True)
+    df.columns = ['date', 'open', 'high', 'low', 'close', 'amount', 'vol']
+    df[['high', 'close']] = df[['close', 'high']]
+    df[['high', 'low']] = df[['low', 'high']]
+    # df['diff'] = ''
+    # df['dea'] = ''
+    # df['macd'] = ''
+    df['date'] = pd.to_datetime(df['date'], format='%Y-%m-%d')
+    if len(normal)>0:
+        normal['date'] = pd.to_datetime(normal['date'], format='%Y-%m-%d')
+        #复牌直接加在后面
+        try:
+            normal_index = df[df['date'] == normal.iat[-2,0]].index.tolist()[-1]
+            normal.drop(normal.tail(1).index, inplace=True)
+        except:normal_index = 0
+        normal = normal.append(df[normal_index+1:],ignore_index=True)
+    else:
+        normal = df
+    normal = ma(normal)
+    normal.to_csv(day_path + code, index=False)
+
+
+
+def macd(df) -> pd.DataFrame:
+    if len(df)<36:
+        return df
+    if 'macd' not in df.columns:
+        df = stock_macd(df)
+
+        return df
+    else:
+        df_temp = df[33:]
+        index = df_temp[df_temp['macd'] == ''].index.tolist()
+        if index!=[]:
+            df_normal = df[index[0]-33:]
+            df_normal = stock_macd(df_normal)
+            df = df[:index[0]].append(df_normal[33:])
+
+        return df
+
+def ma(df):
+    df["ma5"] = talib.MA(df['close'], timeperiod=5)
+    df["ma10"] = talib.MA(df['close'], timeperiod=10)
+    df["ma20"] = talib.MA(df['close'], timeperiod=20)
+    df["ma30"] = talib.MA(df['close'], timeperiod=30)
+    df["ma60"] = talib.MA(df['close'], timeperiod=60)
+    df["ma120"] = talib.MA(df['close'], timeperiod=120)
+    df["ma250"] = talib.MA(df['close'], timeperiod=250)
+
+    return df
+
+if __name__ == '__main__':
+    with open('config.yaml') as f:
+        content = yaml.load(f,Loader=yaml.FullLoader)
+        f.close()
+
+    # df=ef.stock.get_realtime_quotes()
+    # total = len(df)
+    # each = int(total / 6)
+    # p1 = Process(target=update, args=(df.iloc[0:each,0].tolist(),content))
+    # p2 = Process(target=update, args=(df.iloc[each:2 * each,0].tolist(),content))
+    # p3 = Process(target=update, args=(df.iloc[2 * each: 3 * each,0].tolist(),content))
+    # p4 = Process(target=update, args=(df.iloc[3 * each:4 * each,0].tolist(),content ))
+    # p5 = Process(target=update, args=(df.iloc[4 * each:5 * each,0].tolist(),content ))
+    # p6 = Process(target=update, args=(df.iloc[5 * each:total+1,0].tolist(),content ))
+    # p1.start()
+    # p2.start()
+    # p3.start()
+    # p4.start()
+    # p5.start()
+    # p6.start()
+    update('601899',content)
+
+
---
+++ utils/regression_tool.py
@@ -0,0 +1,93 @@
+import pandas as pd
+import talib
+
+import os
+
+from utils.judge_ma import array_ma
+
+
+def csv_resample(df, rule) -> pd.DataFrame:
+    # 重新采样Open列数据
+    df_open = round(df['open'].resample(rule=rule, closed='right', label='left').first(), 2)
+    df_high = round(df['high'].resample(rule=rule, closed='right', label='left').max(), 2)
+    df_low = round(df['low'].resample(rule=rule, closed='right', label='left').min(), 2)
+    df_close = round(df['close'].resample(rule=rule, closed='right', label='left').last(), 2)
+    df_volume = round(df['vol'].resample(rule=rule, closed='right', label='left').sum(), 2)
+    df_amount = round(df['amount'].resample(rule=rule, closed='right', label='left').sum(), 2)
+    # print("新周期数据已生成")
+    # 生成新周期数据
+    df_15t = pd.DataFrame()
+    df_15t = df_15t.assign(open=df_open)
+    df_15t = df_15t.assign(high=df_high)
+    df_15t = df_15t.assign(low=df_low)
+    df_15t = df_15t.assign(close=df_close)
+    df_15t = df_15t.assign(amount=df_amount)
+    df_15t = df_15t.assign(vol=df_volume)
+    # 去除空值
+    df_15t = df_15t.dropna()
+
+    return df_15t
+
+# 读取csv文件，返回pd.DataFrame对象
+def import_csv(df,rule) -> pd.DataFrame:
+
+    df =df.copy()
+    df['date'] = pd.to_datetime(df['date'], format='%Y-%m-%d')
+    df = df.set_index(['date'])
+    df = csv_resample(df, rule)
+    df.reset_index(inplace=True)
+    return df
+
+
+def macd(df):
+    diff, dea, macd = talib.MACD(df["close"],
+                                 fastperiod=12,
+                                 slowperiod=26,
+                                 signalperiod=9)
+    df["diff"] = round(diff, 2)
+    df["dea"] = round(dea, 2)
+    df["macd"] = round(macd * 2, 2)
+    return df
+
+
+def stock_macd(df) -> pd.DataFrame:
+    if len(df)<36:
+        return df
+    if 'macd' not in df.columns:
+        df = macd(df)
+        return df
+    else:
+        df_temp = df[33:]
+        index = df_temp[df_temp['macd'] == ''].index.tolist()
+        if index!=[]:
+            df_normal = df[index[0]-33:]
+            df_normal = stock_macd(df_normal)
+            df = df[:index[0]].append(df_normal[33:])
+
+        return df
+
+
+def test(code,content):
+    real = pd.read_csv(content['normal'] + 'day/'+code)
+    normal = real[:250]
+
+
+    #开始回测
+    for i, row in real[250:].iterrows():
+        normal = normal.append(row).reset_index(drop=True)
+        date = normal['date'].iloc[-1]
+        if date == '2020-01-02':
+            print(1)
+        if array_ma(normal):
+            record = pd.DataFrame(columns=['date', 'key', 'flag', 'temp'])
+            print(normal['date'].iloc[-1])
+        l = pd.DataFrame(
+            {'code':code,'date': normal['date'].iloc[-1] }, index=[1])
+        # if i ==5000:
+        #     print(i)
+
+
+
+
+
+
